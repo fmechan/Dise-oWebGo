aaf586abcfafc80340a2d423c23c8d27319b2c23
francis <fmechan0311@gmail.com>
2021-10-16 20:26:38 -0500

    respaldo8

--- Sesion8/AgendaV2/js/agendav2.js
+++ Sesion8/AgendaV2/js/agendav2.js
@@ -2,7 +2,14 @@ function guardarDato() {
     var nombre = document.getElementById("nombre").value;
     var movil = document.getElementById("movil").value;
     var email = document.getElementById("email").value;
-    localStorage.setItem(nombre, movil, email);
+
+    const datos ={
+        'movil':movil,
+        'email':email,
+    };
+
+    localStorage.setItem(nombre, JSON.stringify(datos));
+
     document.getElementById("nombre").value = "";
     document.getElementById("movil").value = "";
     document.getElementById("email").value = "";
@@ -12,8 +19,12 @@ function guardarDato() {
 
 function recuperarDato() {
     var nombre= document.getElementById("nombre").value;
-    localStorage.getItem(nombre);
-    document.getElementById("movil").value = localStorage.getItem(nombre);
+
+    let datos = localStorage.getItem(nombre);
+    datos = JSON.parse(datos);
+
+    document.getElementById("movil").value = datos.movil;
+    document.getElementById("email").value = datos.email;
 }
 
 function eliminarDato() {
@@ -34,8 +45,13 @@ function actualizarDatos() {
     } else {
         for (var i = 0; i <= localStorage.length - 1; i++) {
             var key = localStorage.key(i);
-            registro += '<li>' + '<span class="nom">' + key + '</span>'
-            + '<span class="nom">' + localStorage.getItem(key) + '</span>' + '</li><br>';
+            let datos = localStorage.getItem(key);
+            datos = JSON.parse(datos);
+
+            registro += `<li> 
+            <span class="nom"> ${key} </span>
+            <span class="nom"> ${datos.movil}</span>
+            <span class="nom datosEmail"> ${datos.email}</span></li><br>`;
         }
     }
     document.getElementById('contactos').innerHTML = registro;
